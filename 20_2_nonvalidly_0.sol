51624e1b7d100f6f0fb63871aa6d55bde59a9bb0
edenarrr <eden.aroch@gmail.com>
2024-04-04 19:54:33 +0200

    ajoute objectif et contraintes de source et destination au .lp

---
+++ 20_2_nonvalidly_0.sol
@@ -0,0 +1,102 @@
+Problem:    
+Rows:       9
+Columns:    61
+Non-zeros:  24
+Status:     OPTIMAL
+Objective:  obj = 188743.5 (MINimum)
+
+   No.   Row name   St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 c_source_11  B              0                         455 
+     2 c_source_18  B              0                        1842 
+     3 c_source_6   B              0                         449 
+     4 c_source_4   B              0                         837 
+     5 c_dest_3     NS           923           923             =          59.5 
+     6 c_dest_5     NS           410           410             =          82.5 
+     7 c_dest_8     NS           192           192             =           272 
+     8 c_dest_19    NS           743           743             =            59 
+     9 c_dest_10    NS          1313          1313             =             3 
+
+   No. Column name  St   Activity     Lower bound   Upper bound    Marginal
+------ ------------ -- ------------- ------------- ------------- -------------
+     1 x_0_4        NL             0             0                       148.5 
+     2 x_4_0        NL             0             0                         159 
+     3 x_0_12       NL             0             0                       141.5 
+     4 x_12_0       NL             0             0                       217.5 
+     5 x_1_7        NL             0             0                       312.5 
+     6 x_7_1        NL             0             0                       284.5 
+     7 x_1_19       NL             0             0                       215.5 
+     8 x_19_1       NL             0             0                         187 
+     9 x_2_13       NL             0             0                         263 
+    10 x_13_2       NL             0             0                         130 
+    11 x_3_19       NL             0             0                        95.5 
+    12 x_19_3       B            923             0               
+    13 x_3_5        B            410             0               
+    14 x_5_3        NL             0             0                          34 
+    15 x_4_6        NL             0             0                          87 
+    16 x_6_4        NL             0             0                          88 
+    17 x_4_11       NL             0             0                         159 
+    18 x_11_4       NL             0             0                       157.5 
+    19 x_4_15       NL             0             0                       308.5 
+    20 x_15_4       NL             0             0                         298 
+    21 x_4_18       NL             0             0                       107.5 
+    22 x_18_4       NL             0             0                         5.5 
+    23 x_5_15       NL             0             0                         163 
+    24 x_15_5       NL             0             0                        61.5 
+    25 x_6_7        NL             0             0                         175 
+    26 x_7_6        NL             0             0                          82 
+    27 x_6_17       NL             0             0                       145.5 
+    28 x_17_6       NL             0             0                          26 
+    29 x_7_13       NL             0             0                       172.5 
+    30 x_13_7       NL             0             0                        48.5 
+    31 x_7_18       NL             0             0                         136 
+    32 x_18_7       NL             0             0                         119 
+    33 x_8_15       NL             0             0                         198 
+    34 x_15_8       B            192             0               
+    35 x_9_10       NL             0             0                         3.5 
+    36 x_10_9       NL             0             0                        55.5 
+    37 x_9_14       NL             0             0                       106.5 
+    38 x_14_9       NL             0             0                       127.5 
+    39 x_9_13       NL             0             0                         242 
+    40 x_13_9       NL             0             0                       158.5 
+    41 x_9_12       NL             0             0                         194 
+    42 x_12_9       NL             0             0                       169.5 
+    43 x_10_19      B            743             0               
+    44 x_19_10      B           1313             0               
+    45 x_10_17      NL             0             0                         131 
+    46 x_17_10      NL             0             0                       171.5 
+    47 x_12_14      NL             0             0                        96.5 
+    48 x_14_12      NL             0             0                       169.5 
+    49 x_12_15      NL             0             0                        57.5 
+    50 x_15_12      NL             0             0                         195 
+    51 x_13_17      NL             0             0                       118.5 
+    52 x_17_13      NL             0             0                       185.5 
+    53 x_14_17      NL             0             0                       199.5 
+    54 x_17_14      NL             0             0                         130 
+    55 x_14_19      NL             0             0                          38 
+    56 x_19_14      NL             0             0                          37 
+    57 x_14_16      NL             0             0                         158 
+    58 x_16_14      NL             0             0                       137.5 
+    59 x_15_19      NL             0             0                       168.5 
+    60 x_19_15      NL             0             0                         195 
+    61 x_16_16      NL             0             0                          42 
+
+Karush-Kuhn-Tucker optimality conditions:
+
+KKT.PE: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+KKT.DE: max.abs.err = 0.00e+00 on column 0
+        max.rel.err = 0.00e+00 on column 0
+        High quality
+
+KKT.DB: max.abs.err = 0.00e+00 on row 0
+        max.rel.err = 0.00e+00 on row 0
+        High quality
+
+End of output
--- generate_model.py
+++ generate_model.py
@@ -8,6 +8,12 @@ class ModelGenerator:
         self.model_type = model_type
         self.nb_items = 0
         self.nb_nodes = 0
+        self.edges = []
+        self.edges_cost = {} # Dictionary that contains the costs of each edge (key is in format start_dest)
+        self.dest = []
+        self.dest_demands = {}
+        self.source = []
+        self.source_capacities = {}
 
     def generate_model(self):
         self.parse_instance_file(self.instance_file)
@@ -18,24 +24,120 @@ class ModelGenerator:
 
     def parse_instance_file(self, instance_file):
         with open("instances/" + instance_file + ".txt", "r") as file:
-            self.nb_items = (file.readline().strip().split(" ")[1])
+            self.nb_items = int((file.readline().strip().split(" ")[1]))
             file.readline()
-            self.nb_nodes = (file.readline().strip().split(" ")[1])
+            self.nb_nodes = int((file.readline().strip().split(" ")[1]))
+
+            edges = {}
+
+            for line in file:
+                self.extract_edge_info(file, line)
+                self.extract_source_info(file, line)
+                self.extract_dest_info(file, line)
+
+
+
+    def extract_edge_info(self, file, line):
+        if line.startswith("EDGES"):
+            nb_edges = int(line.split()[1])
+            next(file)
+            for _ in range(nb_edges):
+                edge_info = next(file).strip().split()
+                edge_id, start, end = edge_info[:3]
+                costs = []
+                for i in range(3, len(edge_info)):
+                    costs.append(edge_info[i])
+
+                cost_avg = 0
+                for cost in costs:
+                    cost_avg += int(cost)
+                cost_avg /= self.nb_items
+                self.edges.append(start + "_" + end)
+                self.edges_cost[start + "_" + end] = cost_avg
+                #print("cost_avg = ", cost_avg)
 
     def generate_aggregated_model(self):
         with open(self.instance_file + "_" + str(self.model_type) + ".lp", "w") as file:
-            file.write("MAXIMIZE\n")
-            objective = "\tobj: \n"
-            file.write(objective)
-
+            # Objective
+            self.write_objective_in_file(file)
+            # Constraints
             file.write("SUBJECT TO\n")
+            self.write_source_constraints_in_file(file)
+            self.write_destination_constraints_in_file(file)
 
             file.write("BINARY\n")
 
             file.write("END\n")
 
+    def write_source_constraints_in_file(self, file):
+        source_constraints = ""
+        for source in self.source:
+            constraint = "\tc_source_" + source + ": "
+            for edge in self.edges:
+                if edge.startswith(source + "_"):  # If the edge ends in 'dest'
+                    edge_end_node = edge.split("_")[1]
+                    constraint += "x_" + str(source) + "_" + str(edge_end_node) + " + "
+            constraint = constraint[:len(constraint) - 3]
+            constraint += " <= "
+            constraint += str(self.source_capacities[source])
+            constraint += "\n"
+            source_constraints += constraint
+        file.write(source_constraints)
+
+    def write_destination_constraints_in_file(self, file):
+        dest_constraints = ""
+        for dest in self.dest:
+            constraint = "\tc_dest_" + dest + ": "
+            for edge in self.edges:
+                if edge.endswith("_" + dest):  # If the edge ends in 'dest'
+                    edge_start_node = edge.split("_")[0]
+                    constraint += "x_" + str(edge_start_node) + "_" + str(dest) + " + "
+            constraint = constraint[:len(constraint) - 3]
+            constraint += " = "
+            constraint += str(self.dest_demands[dest])
+            constraint += "\n"
+            dest_constraints += constraint
+        file.write(dest_constraints)
+
+    def write_objective_in_file(self, file):
+        file.write("MINIMIZE\n")
+        objective = "\tobj: "
+        for edge in self.edges:
+            objective += str(self.edges_cost[edge]) + " x_" + str(edge) + " + "
+        objective = objective[:len(objective) - 2] + "\n"
+        file.write(objective)
+
     def generate_disaggregated_model(self):  # instance_name, nodes, edges, sources, destinations):
         pass
+
+    def extract_source_info(self, file, line):
+        if line.startswith("SOURCES"):
+            nb_source = int(line.split()[1])
+            next(file)
+            for _ in range(nb_source):
+                source_info = next(file).strip().split()
+                source_id = source_info[0]
+                self.source.append(source_id)
+                source_capacity = 0
+                for i in range(1, len(source_info)):
+                    source_capacity += int(source_info[i])
+                self.source_capacities[source_id] = source_capacity
+                #print("Cap = " + str(source_capacity))
+
+
+    def extract_dest_info(self, file, line):
+        if line.startswith("DESTINATIONS"):
+            nb_dest = int(line.split()[1])
+            next(file)
+            for _ in range(nb_dest):
+                dest_info = next(file).strip().split()
+                dest_id = dest_info[0]
+                self.dest.append(dest_id)
+                dest_demand = 0
+                for i in range(1, len(dest_info)):
+                    dest_demand += int(dest_info[i])
+                self.dest_demands[dest_id] = dest_demand
+                #print("Demand = " + str(dest_demand))
 
 
 def main():
